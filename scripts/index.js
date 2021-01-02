bede65b3f3bf02d5964abb735f28d08da9d20d17
mapatie deda <57991126+nikushaa991@users.noreply.github.com>
2021-01-02 21:27:17 +0400

    dynamic background preview + adjustments

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,19 @@ function init() {
 
     document.getElementById('timeSlider').oninput = function() {
         var val = this.value;
-        var primaryBG = 'day';
-        var secondaryBG = 'day';
+        var primaryBG = 'dusk'
+        var secondaryBG = 'night';
 
         switch (true) {
-            case (val < 6):
+            case (val < 250):
                 primaryBG = 'dusk';
                 secondaryBG = 'night';
                 break;
-            case (val < 12):
+            case (val < 500):
                 primaryBG = 'day';
                 secondaryBG = 'dusk';
                 break;
-            case (val < 18):
+            case (val < 750):
                 primaryBG = 'dusk';
                 secondaryBG = 'day';
                 break;
@@ -26,7 +26,7 @@ function init() {
         let secondary = document.body;
 
         primary.style.background = `url('images/${primaryBG}.png')`;
-        primary.style.opacity = 1 / 6 * (val % 6);
+        primary.style.opacity = 1 / 250 * (val % 250);
         primary.style.backgroundPosition = 'center';
 
         secondary.style.background = `url('images/${secondaryBG}.png')`;
