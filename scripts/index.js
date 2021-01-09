a2c37506a382198b1cd08d9874b7f4ca92af71da
mapatie deda <57991126+nikushaa991@users.noreply.github.com>
2021-01-09 22:24:44 +0400

    Transferred to fetch + main page almost done

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,14 @@ var currMinute = (new Date() - new Date().setHours(0, 0, 0, 0)) / 60000;
 
 function init() {
     setupDynamicBackground()
+    let left = getCookie('left')
+    let right = getCookie('right')
+    console.log(left);
+    if (left)
+        requestCity(left, 1, false);
+    if (right)
+        requestCity(right, 3, false);
+    requestCity('Tbilisi', 2, false);
 }
 
 function setupDynamicBackground() {
@@ -45,10 +53,10 @@ function setupDynamicBackground() {
     setInterval(advanceBackground, 60000);
 }
 
-function setupWeatherInfo(id, city, imageName, temp, humidity, clouds) {
+function setupWeatherInfo(id, city, image, temp, humidity, clouds) {
     let bookmark = document.getElementById(`weather-info-${id}`).children;
     bookmark[0].innerHTML = city;
-    bookmark[1].src = 'images/' + imageName + '.png';
+    bookmark[1].src = 'images/' + image + '.png';
     let stats = bookmark[2].children;
     stats[1].innerHTML = temp + '&#176'
     stats[3].innerHTML = humidity + '%';
@@ -56,27 +64,51 @@ function setupWeatherInfo(id, city, imageName, temp, humidity, clouds) {
 }
 
 function searchForCity() {
-    requestCity(document.getElementById('search-textfield').value);
+    requestCity(document.getElementById('search-textfield').value, 4, true);
 }
 
-function requestCity(city) {
-    if (!city) {
-        document.getElementById('weather-info-4').style.visibility = "hidden";
+
+async function requestCity(city, containerIndex, setInvis) {
+    container = `weather-info-${containerIndex}`
+    if (setInvis && !city) {
+        document.getElementById(container).style.visibility = "hidden";
         return;
     }
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-            if (this.responseText === "")
-                setupWeatherInfo(4, 'City Not Found', 'sun', '0', '0', '0')
-            else handleResponse(JSON.parse(this.responseText));
-            document.getElementById('weather-info-4').style.visibility = "visible";
-        }
-    };
-    xhttp.open("GET", `fakeAPI/${city}.txt`, true);
-    xhttp.send();
+    let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e7e7d5d2da4aee3dcb44529c9c69f31a`)
+
+    if (response.ok) {
+        let json = await response.json();
+        handleResponse(json, containerIndex)
+    } else setupWeatherInfo(containerIndex, 'Error', 'sun', '0', '0', '0')
+
+    if (setInvis)
+        document.getElementById(container).style.visibility = "visible";
 }
 
-function handleResponse(response) {
-    setupWeatherInfo(4, response.name, "sun", response.main.temp, response.main.humidity, response.clouds.all)
+function handleResponse(response, containerIndex) {
+    let id = response.weather[0].id
+    var icon = "sun"
+    switch (String(id).charAt(0)) {
+        case '2':
+        case '3':
+        case '5':
+            icon = "rain"
+            break;
+        case '6':
+            icon = "snow";
+            break;
+    }
+    setupWeatherInfo(containerIndex, response.name, icon, response.main.temp, response.main.humidity, response.clouds.all)
+}
+
+
+//COOKIE METHODS
+function setCookie(name, value) {
+    document.cookie = `${name}=${value}; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/`
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
 }
